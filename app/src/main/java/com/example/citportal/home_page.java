d9cb841d2eef5a11867540e72bb860cbbdd099f5
Arvind V <arvind2oxford@gmail.com>
2023-02-10 15:35:07 +0530

    Fragments added

--- app/src/main/java/com/example/citportal/home_page.java
+++ app/src/main/java/com/example/citportal/home_page.java
@@ -1,14 +1,53 @@
 package com.example.citportal;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.example.citportal.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class home_page extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+
+    home home = new home();
+    todo todo = new todo();
+    attendance attendance = new attendance();
+    internals internals = new internals();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+    bottomNavigationView = findViewById(R.id.bottomNavigationView);
+    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,home).commit();
+
+    bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,home).commit();
+                    return true;
+                case R.id.todo:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,todo).commit();
+                    return true;
+                case R.id.attendance:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,attendance).commit();
+                    return true;
+                case R.id.internals:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,internals).commit();
+                    return true;
+            }
+            return false;
+        }
+    });
     }
+
 }
